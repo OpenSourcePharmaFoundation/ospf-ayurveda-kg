76d1888077585f7457a6ecd7f0d98c399e4d6ff1
Smiti Mittal <smiti06@stanford.edu>
2024-01-08 15:47:44 +0530

    edits small

--- data_processing.py
+++ data_processing.py
@@ -3,12 +3,20 @@ import pandas as pd
 import os
 
 # turns local .csv files into pandas dataframes
-def load_data(local_file_paths):
+def load_into_pd(local_file_paths):
     file_df_dict = {}
     for file_path in local_file_paths:
         file_df = pd.read_csv(file_path)
         file_df_dict[os.path.basename(file_path).split('.')[0]] = file_df
     return file_df_dict
+
+# loads ayurvedic_formulation.csv into neo4j
+def load_ayurvedic_formulation():
+    
+
+# loads all files necessary into neo4j
+def load_into_neo4j():
+    load_ayurvedic_formulation()
 
 # scraping data
 
@@ -19,7 +27,3 @@ def load_data(local_file_paths):
 # graph
 
 local_file_paths = ['./ayurvedic_formulation.csv']
-file_df_dict = load_data(local_file_paths)
-
-ayurveda_df = file_df_dict['ayurvedic_formulation']
-print(ayurveda_df)
