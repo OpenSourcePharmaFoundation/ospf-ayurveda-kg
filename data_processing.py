1cb0e5c575e843e07a454fd222dabd338bb3e2f9
Smiti Mittal <smiti06@stanford.edu>
2024-02-05 15:56:19 +0530

    getting pubchem urls from imppat chemicals

--- data_processing.py
+++ data_processing.py
@@ -4,6 +4,7 @@ import os
 from bs4 import BeautifulSoup as beaut
 import requests
 import json
+import csv
 
 # turns local .csv files into pandas dataframes
 def load_into_pd(local_file_paths):
@@ -35,8 +36,38 @@ def ayur_form_to_IMPPAT_url_ther(csv_path):
         url_list.append(url)
     return(url_list)
 
+# prepares pubchem urls for scraping taking in the path name for the phytochemicals csv
+def phytochem_to_pubchem_url(url_list_imppat, csv_path):
+    phytochem_dict={}
+    for url in url_list_imppat:
+        r = requests.get(url) 
+        
+        # parsing html
+        soup = beaut(r.content, 'html.parser') 
+        table_phytochem = soup.find('table',id='table_id')
+        for j in table_phytochem.find_all('tr')[1:]:
+            row_data = j.find_all('td')
+            row = [i.text for i in row_data]
+            phytochem_dict[row[3]]=row[2]
+    imppat_url_list_detail = []
+    for id in phytochem_dict.values():
+        url = 'https://cb.imsc.res.in/imppat/phytochemical-detailedpage/' + id
+        imppat_url_list_detail.append(url)
+
+    url_list = []
+    for imppat_url in imppat_url_list_detail:
+        r = requests.get(imppat_url) 
+
+        # parsing html
+        soup = beaut(r.content, 'html.parser')
+        summary = soup.find('div',{"class":"col-8 pt-0 mt-0 ml-2 pl-2"})
+        url_data = summary.find_all('a', href=True)
+        url_list.append(url_data[0]['href'])
+    return(url_list)
+    
+
 # scrape phytochemical data based on list of urls from IMPPAT
-def url_IMPPAT_chem_to_json(url_list, json_path, data_name):
+def url_IMPPAT_chem_pn(url_list, json_path, data_name):
     phytochem_dict={}
     
     for url in url_list:
@@ -88,11 +119,18 @@ def url_IMPPAT_ther_to_json(url_list, json_path):
     } for key, value in ther_dict.items()]
     with open(json_path, "w") as file:
         json.dump(d, file)
-# cleaning and processing data 
 
-# data structure
-
-# graph
+# scrape chemical-gene target interaction data based on list of urls from pubchem
+'''def url_pubchem_chem_target_json(url_path, csv_path):
+    file_df = pd.read_csv(url_path)
+    for row in file_df.iterrows():
+        r = requests.get(url) 
+        if r.status_code!=200:
+            print(url)
+        else:
+            print('success')
+            
+'''
 
 ## scraping IMPPAT phytochemical data based on taxonomical plant names
 '''url_list_chem = ayur_form_to_IMPPAT_url_chem('./ayurvedic_formulation.csv')
@@ -116,3 +154,15 @@ for index, row in df.iterrows():
     print(df_new.loc[index, 0])
 df_new.to_csv("phytochemicals_cleaned.csv", header=None, index=None) 
 '''
+
+## scraping pubchem chemical-target interaction data based on phytochemical names
+url_list_chem = ayur_form_to_IMPPAT_url_chem('./ayurvedic_formulation.csv')
+url_list = phytochem_to_pubchem_url(url_list_chem,'./phytochemicals_cleaned.csv')
+ 
+# opening the csv file in 'w+' mode
+file = open('pubchem_urls.csv', 'w+', newline ='')
+with file:    
+    write = csv.writer(file)
+    write.writerows(url_list)
+
+#url_pubchem_chem_target_json('pubchem_urls.csv', 'phytochem_gene_interactions.csv')
