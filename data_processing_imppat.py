59b8eb82c396fe7a347f9ca427c9ef0c5cd414d7
Smiti Mittal <smiti06@stanford.edu>
2024-05-06 12:35:22 +0530

    updates

--- data_processing_imppat.py
+++ data_processing_imppat.py
@@ -43,9 +43,36 @@ def url_IMPPAT_chem_pn(url_list, json_path, data_name):
     with open(json_path, "w") as file:
         json.dump(d, file)
 
+# scrape phytochemical-part data based on list of urls from IMPPAT
+def url_IMPPAT_chem_part(url_list, json_path, data_name):
+    phytochem_dict={}
+    
+    for url in url_list:
+        r = requests.get(url) 
+        plant_url = url.split('/')[-1]
+        plant_name = plant_url.split('%')[0] + ' ' + plant_url.split('20')[-1]
+        
+        # parsing html
+        soup = beaut(r.content, 'html.parser') 
+        table_phytochem = soup.find('table',id='table_id')
+        phytochem_list=[]
+        for j in table_phytochem.find_all('tr')[1:]:
+            row_data = j.find_all('td')
+            row = [i.text for i in row_data]
+            phytochem_list.append((row[1],row[3]))
+        phytochem_dict[plant_name]=phytochem_list
+    
+    # write json
+    d = [{ 
+    "plant": key,
+    data_name: value
+    } for key, value in phytochem_dict.items()]
+    with open(json_path, "w") as file:
+        json.dump(d, file)
+
 ## scraping IMPPAT phytochemical data based on taxonomical plant names
 url_list_chem = ayur_form_to_IMPPAT_url_chem('./ayurvedic_formulation.csv')
-url_IMPPAT_chem_to_json(url_list_chem, 'plant_phytochemicals.json', "phytochemicals")
+url_IMPPAT_chem_part(url_list_chem, 'plant_phytochemicals_parts.json', "phytochemicals")
 
         
 
