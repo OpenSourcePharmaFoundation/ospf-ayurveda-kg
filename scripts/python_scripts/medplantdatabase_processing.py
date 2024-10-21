bb598eae10ecd4778e5d56afd34c2144cd8ce9e8
JayDub302 <159957188+JayDub302@users.noreply.github.com>
2024-10-21 13:28:22 -0700

    Preparing URLs from Botanical Survey of India Medicinal Plant Database for scraping.
    Work in progress.

--- scripts/python_scripts/medplantdatabase_processing.py
+++ scripts/python_scripts/medplantdatabase_processing.py
@@ -1,3 +1,14 @@
+#import dependencies
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup as beaut
+import json
+
+#prepares urls from medicinal plant database of India for scraping to gather phytochemical data, taking in the path name for the 'ayurvedic formulations' csv.
+def ayur_form_to_medplantdata_url(csv_path):
+  url_list = []
+  file_df = pd.read_csv(csv_path)
+  for row in file_df.iterrows():
+    sci_name = row[1]['Scientific name of the ingredient']
+    sci_name_split = sci_name.split(' ')
+    
